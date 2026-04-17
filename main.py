db7c3da5e0766f60032dca911064a2c88b5e6168
zecu32 <kj181977@gmail.com>
2026-04-17 17:08:42 +0900

    add dotenv

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import os
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
+# 環境変数の確認
+client_id = os.getenv("FREEE_CLIENT_ID")
+client_secret = os.getenv("FREEE_CLIENT_SECRET")
+
+print(f"Client ID: {client_id}")
+print(f"Client Secret: {client_secret}")
